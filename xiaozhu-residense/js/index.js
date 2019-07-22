8ed870151afa42fe2c5c19b23b6a5e9472fee1ee
liaodehui <1959956996@qq.com>
2019-07-22 10:00:31 +0800

    小猪民宿学习

---
+++ xiaozhu-residense/js/index.js
@@ -0,0 +1,12 @@
+(function () {
+    var length = $('.banner__li').length
+    setInterval(autoPlay,6000)
+    var current=0
+    function autoPlay(){
+        let next=current<length-1?current+1:0
+        $('.banner__li').eq(next).fadeIn(3000)
+        $('.banner__li').eq(next).attr('style','display:list-item')
+        $('.banner__li').eq(current).fadeOut(3000)
+        current=next
+    }
+})()
---
+++ xiaozhu-residense/readme.md
@@ -0,0 +1,9 @@
+- fadeIn和fadeOut
+    fadeIn(time,callback) opacity从0增大到1
+    fadeOut(time,callback) opacity从1减小到0
+    同时开始opacity变化
+- absolute
+    元素会脱离文档流，因此图片会在一行，切换时不会出现空白阶段
+- 父容器设置高度等于行高实现垂直居中，子元素继承父元素行高
+- background-position 为背景图片左上角在的定位
+- iconfont
