8c71461232e8d64e5c2cf4eb48970195dc8edc2d
liaodehui <1959956996@qq.com>
2019-09-18 20:12:23 +0800

    vue-cli中使用全局stylus

---
+++ vue/vue-styl/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello bgc">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+<style lang="stylus" scoped>
+// @import '../assets/stylus/mixin'
+  .hello
+    height 20px
+    border-1px(red)
+    margin-top $mt
+</style>
